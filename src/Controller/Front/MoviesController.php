c7162ccab008e8432a87c5b8145d5ceabaf53251
hugoresende27 <hugoresende27@gmail.com>
2024-10-01 19:07:31 +0100

    tuto_4

---
+++ src/Controller/Front/MoviesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Front;
+
+use App\Services\TMDBService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MoviesController extends AbstractController
+{
+
+    private TMDBService $tMDBService;
+    public function __construct(TMDBService $TMDBService)
+    {
+        $this->tMDBService = $TMDBService;
+    }
+    #[Route('/movies', name: 'app_front_movies')]
+    public function index(): Response
+    {
+        $movies = $this->tMDBService->getMovies();
+
+        return $this->render('front/movies/index.html.twig', [
+            'movies' => $movies['results'] ?? [],
+        ]);
+    }
+}
---
+++ src/Services/TMDBService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7\Request;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class TMDBService
+{
+    private ParameterBagInterface $params;
+    private string $baseUrl;
+    private string $apiKey;
+    public function __construct(ParameterBagInterface $params)
+    {
+        $this->baseUrl = $params->get("api.tmdb_base_url");
+        $this->apiKey = $params->get("api.tmdb_apikey");
+    }
+
+
+    
+    public function getMovies()
+    {
+        return $this->callApi('3/discover/movie');
+    }
+
+
+
+    public function callApi(string $url, string $method = 'GET')
+    {
+        try {
+            $url = $this->baseUrl . $url ."?api_key=".$this->apiKey;
+            $client = new Client();
+            $request = new Request($method, $url);
+            $response = $client->send($request);
+            $decodedResponse = json_decode($response->getBody()->getContents(), true);
+            return $decodedResponse;
+        } catch (GuzzleException $e) {
+            return ['message' => $e->getMessage(), 'code' => $e->getCode()];
+        }
+    }
+}
