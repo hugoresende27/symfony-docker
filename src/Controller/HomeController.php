82eedaa31ea94d9bd4a9103153fec57451e977b2
hugoresende <hugo.resende@iservices.pt>
2024-10-01 11:52:14 +0100

    login v1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,7 @@ class HomeController extends AbstractController
     #[Route(path: '/', name: 'app_home')]
     public function index(): Response
     {
+ 
         return $this->render('home/index.html.twig', [
             'title' => 'Hello Devs !!!',
         ]);
